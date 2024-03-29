0f59925464c62bff935f578f483d18a2be86fe38
Your Name <you@example.com>
2024-03-29 01:41:07 +0000

    new try

--- list.c
+++ list.c
@@ -97,7 +97,21 @@ void pushBack(List * list, void * data) {
 
 void pushCurrent(List * list, void * data) 
 {
-  return NULL;
+  Node* nuevoNodo = createNode(data);
+  if(list->current == NULL)
+  {
+    pushFront(list,data); //Verifica que sea una lista vacía y lo pone en el primer elemento
+  }
+  else
+  {
+    nuevoNodo->prev = list->current;
+    nuevoNodo->next = list->current->next;
+    if(list->current->next == NULL) //En caso de ser el último elemento (current) ponemos que tail = nuevoNodo
+    {
+      list->tail = nuevoNodo;
+    }
+    list->current->next = nuevoNodo;
+  }
 }
 
 void * popFront(List * list) {
